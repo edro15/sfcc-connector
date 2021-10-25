2e4aa24d5a6ec5e468ee03d100f93997a3a96504
Erica Pescio <edro15@users.noreply.github.com>
2021-10-25 14:52:51 +0200

    Bumped version v1.0.1 --> v1.0.2

--- sfcc_connector/__main__.py
+++ sfcc_connector/__main__.py
@@ -1,7 +1,7 @@
 __title__ = "sfcc connector"
 __description__ = "Python connector for Salesforce Commerce Cloud via Open Commerce API"
 __url__ = "https://github.com/edro15/sfcc-connector"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Erica Pescio"
 __author_email__ = "edro15@users.noreply.github.com"
 __license__ = "MIT"
